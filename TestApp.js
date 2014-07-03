30611dac9ab67251c6706fe7a7ddbb022174ee16
Kahmali Rose <krose72205@gmail.com>
2014-07-03 06:28:24 -0400

    Show message times in client locale
    - Add a Template.helper function to show all message times in the client
      locale, as opposed to the locale of the user that posted the message
    - Clean up the code a bit
      - More comments
      - Combine all the Template.events into a single JS object

--- TestApp.js
+++ TestApp.js
@@ -2,51 +2,55 @@ Messages = new Meteor.Collection("messages");
 
 if (Meteor.isClient) {
 
-	// Send button click event
-  Template.main.events({
-    'click #send_button' : function () {
-			if (console !== "undefined") {    
-    		console.log("Sending message: " + $("#message_input").val());
-			} 	
-			name = $("#name_input").val() || "<<anonymous>>";
-      Messages.insert(
-				{name : name,
-				 content : $("#message_input").val(), 
-				 likes : 0,
-				 time : new Date().getTime(),
-				 user_time : new Date().toLocaleString()
-        }
-      ); 
-      $("#message_input").val(''); 
-   }
-  });  
-  
-	// Clear button click event
-	Template.main.events({
+    // Setup any click events in this template
+    Template.main.events({
+	    // Send button click event
+        'click #send_button' : function () {
+	        if (console !== "undefined") {    
+	            console.log("Sending message: " + $("#message_input").val());
+	        } 	
+	        name = $("#name_input").val() || "<<anonymous>>";
+            Messages.insert({
+                name : name,
+		        content : $("#message_input").val(), 
+		        likes : 0,
+		        time : new Date().getTime()
+            }); 
+            $("#message_input").val(''); 
+        },
+                
+	    // Clear button click event
 		'click #clear_button' : function() {
 			Meteor.call('clear_all_messages');	
-		}
-	});
-
-	// Like button click event
-	Template.main.events({
+		},
+		
+	    // Like button click event
 		'click .like_button' : function() {
 			Messages.update(this._id, {$inc : {likes : 1}} );
 		}
 	});
+    
+    // Setup any helper functions for this template
+    Template.main.helpers({
+        // Get the message time in the client locale
+        'locale_time' : function(epoch_time) {
+            return (new Date(epoch_time)).toLocaleString()
+        } 
+    });
 
-
-  Template.main.message_list = function() {
-			return Messages.find({}, {sort : {time : -1}});
-	}
+    // Get all messages, in descending chronological order
+    Template.main.message_list = function() {
+		return Messages.find({}, {sort : {time : -1}});
+    }
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+    Meteor.startup(function () {
 		return Meteor.methods({
+		    // Removes all messages from the db
 			'clear_all_messages' : function() {
 				return Messages.remove({});
 			}
-  	});
+  	    });
 	});
 }
